e36f4a7ce87f30343e87aede9aa4b65d7753872e
rimelucci <rimelucci@gmail.com>
2022-01-28 01:52:39 -0500

    added routing and centered login

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Form, Input, Button, Checkbox, Row, Typography, Col } from 'antd';
+import { Feed } from './Feed';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './App.css';
 
-function App() {
+const { Title } = Typography;
+
+const App = () => (
+  <div className="App">
+    <Routes>
+      <Route path='/' element={<NormalLoginContainer/>} />
+      <Route exact path='/feed' element={<Feed/>} />
+    </Routes>
+  </div>
+);
+
+
+
+const NormalLoginContainer = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div style={{minHeight: "100%"}}>
+        <Row justify="center" align="middle" style={{height: "100vh"}}>
+          <div style={{ display: "flex", width: "100%", flexDirection: "column", alignItems: "center"}}>
+            <Title level={2} >Vouch</Title>
+            <NormalLoginForm/>
+          </div>
+        </Row>
+    // </div>
   );
-}
+};
+
+
+const NormalLoginForm = () => {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button" href="/feed">
+            Log in
+          </Button>
+          <br/>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
 
 export default App;
---
+++ antd-demo/src/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+
+export const Feed = () => (
+  <div className="App">
+    <h1>yurrr</h1>
+    <Button href="/">Take me back yurr</Button>
+  </div>
+);
+
+
+export default Feed;
